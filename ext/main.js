546c94164a3c11e51d8c40bcb061a4921363083e
unknown <v626284@LR9E28B2.EMEA.AD.JPMORGANCHASE.com>
2014-08-27 22:47:46 +0100

    magic selector

--- ext/main.js
+++ ext/main.js
@@ -1,7 +1,7 @@
 var tags = document.getElementsByTagName("*");
 var RDFaPageAttributes = ["typeof", "about", "prefix", "vocab", "resource", "property", "inlist", "datatype"];
 //var MicroformatsPageAttributes = ["rel", "rev", "class"];
-var MicroDataPageAttributes = ["itemscope", "itemtype" "itemid", "itemprop", "itemref"];
+var MicroDataPageAttributes = ["itemscope", "itemtype", "itemid", "itemprop", "itemref"];
 
 for ( var i = 0; i < tags.length; i++) {
 	var attributes = tags[i].attributes;
@@ -9,6 +9,7 @@ for ( var i = 0; i < tags.length; i++) {
 		if (RDFaPageAttributes.indexOf(attributes[j].name) != -1) {
 			console.info("This is a RDFa web page " + tags[i].tagName);
 		}
+
 //		if (MicroformatsPageAttributes.indexOf(attributes[j].name) != -1) {
 //		    console.info("This is a Microformats web page " + tags[i].tagName);
 //		}
@@ -17,3 +18,21 @@ for ( var i = 0; i < tags.length; i++) {
 //		}
 	}
 }
+
+console.info($('[typeof]:not([typeof] [typeof])'));
+
+console.info($( ".h-product" ).parent().html());
+
+    $(body).find("[typeof]").each(function() {
+              var request = $.ajax({
+                url: "http://any23.org/any23/json/",
+                type: "POST",
+                data: $(this).parent().html(),
+                contentType:  "text/html"
+              });
+
+              request.done(function( msg ) {
+                console.info(msg);
+              });
+    });
+
