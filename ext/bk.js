c39d2607cc0c706987ab36f09ea83fdb08d94951
unknown <v626284@LR9E28B2.EMEA.AD.JPMORGANCHASE.com>
2014-08-30 11:39:16 +0100

    clear storage

--- ext/bk.js
+++ ext/bk.js
@@ -1,6 +1,7 @@
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
 	chrome.storage.sync.get('tags', function(items) {
+	    chrome.storage.sync.clear();
   		chrome.storage.sync.set({'Microformats': JSON.stringify(JSON.parse(request.tags).Microformats)} ,function() {
   		});
         chrome.storage.sync.set({'Microdata': JSON.stringify(JSON.parse(request.tags).Microdata)} ,function() {
